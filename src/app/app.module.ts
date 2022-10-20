637ad8dceb3360d615497342960546e920e74f02
Manuel Lao <manolinlao@gmail.com>
2022-10-20 14:03:13 +0200

    fin chapter 12- forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { ProductComponent } from './components/product/product.component';
+import {AppComponent} from './app.component';
+import {ProductComponent} from './components/product/product.component';
+import { ProductOld01Component } from './components/product-old01/product-old01.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, ProductComponent, ProductOld01Component],
+    imports: [BrowserModule, FormsModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,22 +1,15 @@
 import {Component, OnInit} from '@angular/core';
 import {Product} from 'src/app/model/product.model';
 import {Model} from '../../model/repository.model';
+import {NgForm, NgModel, ValidationErrors} from '@angular/forms';
 
 @Component({
     selector: 'app-product',
     templateUrl: './product.component.html',
     styleUrls: ['./product.component.css'],
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent {
     model: Model = new Model();
-
-    constructor() {}
-
-    ngOnInit(): void {}
-
-    getProductByPosition(position: number): Product {
-        return this.model.getProducts()[position];
-    }
 
     getProduct(key: number): Product | undefined {
         return this.model.getProduct(key);
@@ -26,46 +19,59 @@ export class ProductComponent implements OnInit {
         return this.model.getProducts();
     }
 
-    getProductCount(): number {
-        return this.model.getProducts().length;
+    newProduct: Product = new Product();
+
+    get jsonProduct() {
+        return JSON.stringify(this.newProduct);
     }
 
-    targetName: string = 'kayak';
-
-    ///////////////////////////////// demo de clases
-
-    getClasses(id: number): string {
-        let product = this.model.getProduct(id);
-        return 'p-2 ' + ((product?.price ?? 0) < 50 ? 'bg-info' : 'bg-warning');
+    addProduct(p: Product) {
+        console.log('New Product: ' + this.jsonProduct);
     }
 
-    /* para trabajar con ngClass */
-    /* retorna un objeto Map -> clave-valor
-     del estilo:
-     {
-      'text-center bg-danger':true,
-      'bg-info':false
-     }
-
-  */
-    getClassMap(id: number): Object {
-        let product = this.model.getProduct(id);
-        return {
-            'text-center bg-danger': product?.name == 'kayak',
-            'bg-info': (product?.price ?? 0) < 50,
-        };
+    getMessages(errs: ValidationErrors | null, name: string): string[] {
+        let messages: string[] = [];
+        for (let errorName in errs) {
+            switch (errorName) {
+                case 'required':
+                    messages.push(`You must enter a ${name}`);
+                    break;
+                case 'minlength':
+                    messages.push(`A ${name} must be at least
+                        ${errs['minlength'].requiredLength}
+                        characters`);
+                    break;
+                case 'pattern':
+                    messages.push(`The ${name} contains
+                         illegal characters`);
+                    break;
+            }
+        }
+        return messages;
     }
 
-    /*
-    para trabajar con ngStyle
-      [ngStyle]='getStyles(2)'
-   */
-    getStyles(id: number): Object {
-        let product = this.model.getProduct(id);
-        return {
-            fontSize: '30px',
-            'margin.px': 100,
-            color: (product?.price ?? 0) > 50 ? 'red' : 'green',
-        };
+    getValidationMessages(state: NgModel, thingName?: string) {
+        let thing: string = state.path?.[0] ?? thingName;
+        return this.getMessages(state.errors, thing);
+    }
+
+    formSubmitted: boolean = false;
+
+    submitForm(form: NgForm) {
+        this.formSubmitted = true;
+        if (form.valid) {
+            this.addProduct(this.newProduct);
+            this.newProduct = new Product();
+            form.resetForm();
+            this.formSubmitted = false;
+        }
+    }
+
+    getFormValidationMessages(form: NgForm): string[] {
+        let messages: string[] = [];
+        Object.keys(form.controls).forEach((k) => {
+            this.getMessages(form.controls[k].errors, k).forEach((m) => messages.push(m));
+        });
+        return messages;
     }
 }
