c62ef45d1f80f3d0933a36048ee3d35e229e021c
Manuel Lao <manolinlao@gmail.com>
2022-10-25 11:06:41 +0200

    fin chapter13

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {ProductComponent} from './components/product/product.component';
-import { ProductOld01Component } from './components/product-old01/product-old01.component';
+import {ProductOld01Component} from './components/product-old01/product-old01.component';
+import {ProductOldChapter12Component} from './components/product-old-chapter12/product-old-chapter12.component';
+import { PaAttrDirective } from './directives/pa-attr.directive';
 
 @NgModule({
-    declarations: [AppComponent, ProductComponent, ProductOld01Component],
+    declarations: [AppComponent, ProductComponent, ProductOld01Component, ProductOldChapter12Component, PaAttrDirective],
     imports: [BrowserModule, FormsModule],
     providers: [],
     bootstrap: [AppComponent],
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -21,57 +21,11 @@ export class ProductComponent {
 
     newProduct: Product = new Product();
 
-    get jsonProduct() {
-        return JSON.stringify(this.newProduct);
-    }
-
     addProduct(p: Product) {
-        console.log('New Product: ' + this.jsonProduct);
+        this.model.saveProduct(p);
     }
 
-    getMessages(errs: ValidationErrors | null, name: string): string[] {
-        let messages: string[] = [];
-        for (let errorName in errs) {
-            switch (errorName) {
-                case 'required':
-                    messages.push(`You must enter a ${name}`);
-                    break;
-                case 'minlength':
-                    messages.push(`A ${name} must be at least
-                        ${errs['minlength'].requiredLength}
-                        characters`);
-                    break;
-                case 'pattern':
-                    messages.push(`The ${name} contains
-                         illegal characters`);
-                    break;
-            }
-        }
-        return messages;
-    }
-
-    getValidationMessages(state: NgModel, thingName?: string) {
-        let thing: string = state.path?.[0] ?? thingName;
-        return this.getMessages(state.errors, thing);
-    }
-
-    formSubmitted: boolean = false;
-
-    submitForm(form: NgForm) {
-        this.formSubmitted = true;
-        if (form.valid) {
-            this.addProduct(this.newProduct);
-            this.newProduct = new Product();
-            form.resetForm();
-            this.formSubmitted = false;
-        }
-    }
-
-    getFormValidationMessages(form: NgForm): string[] {
-        let messages: string[] = [];
-        Object.keys(form.controls).forEach((k) => {
-            this.getMessages(form.controls[k].errors, k).forEach((m) => messages.push(m));
-        });
-        return messages;
-    }
+    submitForm() {
+        this.addProduct(this.newProduct);
+    }    
 }
